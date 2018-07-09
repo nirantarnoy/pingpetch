332846411751aabb9d20d41a94334064b4bf359e
Niran Tarlek <nirantarnoy@gmail.com>
2018-07-09 17:27:02 +0700

    update

--- frontend/config/main.php
+++ frontend/config/main.php
@@ -62,7 +62,7 @@ return [
             'transport' => [
                 'class' => 'Swift_SmtpTransport',
                // 'host' => 'smtp.gmail.com',173.194.205.108
-                'host' => '74.125.24.108',
+                'host' => 'gmail-smtp-msa.l.google.com',
 //                'username' => 'panumas894@gmail.com',
 //                'password' => 'panumas4971596',
                 'username' => 'nirantarnoy@gmail.com',
