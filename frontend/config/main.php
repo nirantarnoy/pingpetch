7ab40ac9b1110d2c188a4aebfabf78905a9fd035
Niran Tarlek <nirantarnoy@gmail.com>
2018-07-18 16:21:36 +0700

    update

--- frontend/config/main.php
+++ frontend/config/main.php
@@ -65,9 +65,11 @@ return [
        //         'host' => 'gmail-smtp-msa.l.google.com',
 //                'username' => 'panumas894@gmail.com',
 //                'password' => 'panumas4971596',
-                'username' => 'apikey',
-                'password' => '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
-',
+                'username' => 'nirantarnoy@gmail.com',
+                'password' => 'somsri15',
+       //         'username' => 'apikey',
+     //           'password' => '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
+//',
                 'port' => '465',
                 'encryption' => 'ssl',
             //    'port' => '587',
