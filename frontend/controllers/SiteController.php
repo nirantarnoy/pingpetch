e300081c973fba62a4ab45cb008e9e5f097847f4
Niran Tarlek <nirantarnoy@gmail.com>
2018-07-18 16:38:08 +0700

    update

--- frontend/controllers/SiteController.php
+++ frontend/controllers/SiteController.php
@@ -233,7 +233,6 @@ class SiteController extends Controller
                    ])
                    ->setFrom(['panumas894@gmail.com'=>'Pingpetch Web'])
                        ->setTo('pingpetch168@gmail.com')
-                    //   ->setTo('niran_tarlek@hotmail.com')
                        ->setSubject($model->title)
                    ->send();
 
